b62355daf7f72198d4559fc2071cf619d9f5eeca
Matthew L. Fidler <matthew.fidler@gmail.com>
2013-10-18 15:20:42 -0500

    Small changes to default layout.

--- ergoemacs-themes.el
+++ ergoemacs-themes.el
@@ -114,6 +114,7 @@
     
     ;; keyword completion, because Alt+Tab is used by OS
     ("M-t" ergoemacs-call-keyword-completion "↯ compl")
+    ("M-T" flyspell-auto-correct-word "flyspell")
     
     ;; Hard-wrap/un-hard-wrap paragraph
     ("M-q" ergoemacs-compact-uncompact-block "fill/unfill ¶")
@@ -190,6 +191,7 @@
     ("<apps> t" (helm-mini ido-switch-buffer switch-to-buffer) "switch buf")
     ("<apps> z" undo "↶ undo")
     ("<apps> n a" org-agenda "agenda")
+    ("<apps> n t" org-capure "capture")
     ("<apps> n c" calc "calc" t)
     ("<apps> n m" magit-status "magit" t)
     ("<apps> n g" grep "grep" t)
@@ -471,26 +473,7 @@
     (Info-mode-hook
      (("<backspace>" Info-history-back)
       ("<S-backspace>" Info-history-forward)))
-    ;; (when (or (not (featurep 'xemacs)) (featurep 'rtf-support))
-    ;;   (global-set-key [(control f1)] 'ess-sas-rtf-portrait)
-    ;;   (global-set-key [(control f2)] 'ess-sas-rtf-landscape))
-    ;; (global-set-key (quote [f2]) 'ess-revert-wisely)
-    ;; (global-set-key (quote [f3]) 'ess-sas-goto-shell)
-    ;; (global-set-key (quote [f4]) 'ess-sas-goto-file-1)
-    ;; (global-set-key (quote [f5]) 'ess-sas-goto-sas)
-    ;; (global-set-key (quote [f6]) 'ess-sas-goto-log)
-    ;; (global-set-key [(control f6)] 'ess-sas-append-log)
-    ;; (global-set-key (quote [f7]) 'ess-sas-goto-lst)
-    ;; (global-set-key [(control f7)] 'ess-sas-append-lst)
-    ;; (global-set-key (quote [f8]) 'ess-sas-submit)
-    ;; (global-set-key [(control f8)] 'ess-sas-submit-region)
-    ;; (global-set-key (quote [f9]) 'ess-sas-data-view-fsview)
-    ;; (global-set-key [(control f9)] 'ess-sas-data-view-insight)
-    ;; (global-set-key (quote [f10]) 'ess-sas-toggle-sas-log-mode)
-    ;; (global-set-key [(control f10)] 'ess-sas-toggle-sas-listing-mode)
-    ;; (global-set-key (quote [f11]) 'ess-sas-goto-file-2)
-    ;; (global-set-key [(control f11)] 'ess-ebcdic-to-ascii-search-and-replace)
-    ;; (global-set-key (quote [f12]) 'ess-sas-graph-view)
+    
     (SAS-mode-hook
      (("<apps> j j" ess-sas-goto-log nil t)
       ("<apps> j u" ess-sas-goto-lst nil t)
