7e53a05fe5ab0c3d9e6ffe0616fa63d4c2d01e9c
Matthew L. Fidler <matthew.fidler@gmail.com>
2013-11-25 10:18:46 -0600

    Version bump.

--- ergoemacs-mode.el
+++ ergoemacs-mode.el
@@ -149,7 +149,7 @@
 (load "ergoemacs-layouts")
 
 ;; Ergoemacs-keybindings version
-(defconst ergoemacs-mode-version "5.13.11"
+(defconst ergoemacs-mode-version "5.13.11-1"
   "Ergoemacs-keybindings minor mode version number.")
 
 (defconst ergoemacs-mode-changes "Delete window Alt+0 changed to Alt+2.
