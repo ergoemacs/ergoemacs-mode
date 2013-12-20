e6438d9db485674e8156d9772573da4eb1d31d5c
Matthew L. Fidler <matthew.fidler@gmail.com>
2013-12-20 12:52:46 -0600

    Release 5.13.12-2: - Added option to allow setup of ergoemacs keys every time a hook is   run instead of just once. - Fix escape to work with org-mode date/time stamps - Fixed handling of prefixes that caused Ctrl+U Alt+a to eat the first   character:   https://github.com/ergoemacs/ergoemacs-mode/issues/133 - Fixed shift-selection-mode:   https://github.com/ergoemacs/ergoemacs-mode/issues/137

--- ergoemacs-mode.el
+++ ergoemacs-mode.el
@@ -150,7 +150,7 @@
   (load "ergoemacs-layouts"))
 
 ;; Ergoemacs-keybindings version
-(defconst ergoemacs-mode-version "5.13.12-1"
+(defconst ergoemacs-mode-version "5.13.12-2"
   "Ergoemacs-keybindings minor mode version number.")
 
 (defconst ergoemacs-mode-changes "Delete window Alt+0 changed to Alt+2.
