c7dd7de5f69df3db50daa5d9b1816eaaa19d2810
Matthew L. Fidler <matthew.fidler@gmail.com>
2014-04-30 12:13:05 -0500

    Add Test for Issue #202

--- ergoemacs-test.el
+++ ergoemacs-test.el
@@ -245,6 +245,14 @@ sunt in culpa qui officia deserunt mollit anim id est laborum.")
                                                            "<apps> m c"
                                                          "<menu> m c") 'define-key nil "<menu>") t)))
 
+(ert-deftest ergoemacs-test-global-key-set-m-semi-before ()
+  "Test setting M-; before loading."
+  (should (equal (ergoemacs-test-global-key-set-before nil "M-;") t)))
+
+(ert-deftest ergoemacs-test-global-key-set-m-semi-after ()
+  "Test setting M-; before loading."
+  (should (equal (ergoemacs-test-global-key-set-before t "M-;") t)))
+
 (ert-deftest ergoemacs-test-global-key-set-apps-before ()
   "Test setting <apps> before loading."
   (should
@@ -928,7 +936,6 @@ Selected mark would not be cleared after paste."
         (old-ergoemacs-theme ergoemacs-theme)
         (old-ergoemacs-keyboard-layout ergoemacs-keyboard-layout)
         (ret nil))
-    ;; Setup input decode map just like `xterm' for some common keys.
     (setq input-decode-map (make-sparse-keymap)
           ergoemacs-theme nil
           ergoemacs-keyboard-layout "us")
@@ -943,11 +950,12 @@ Selected mark would not be cleared after paste."
     (save-excursion
       (switch-to-buffer (get-buffer-create "*ergoemacs-test*"))
       (delete-region (point-min) (point-max))
+      ;; Setup input decode map just like `xterm' for some common keys.
       (insert ergoemacs-test-lorem-ipsum)
       (goto-char (point-max))
       (beginning-of-line)
       (with-timeout (0.2 nil)
-        (read-key "M-O A"))
+        (ergoemacs-read-key "M-O A"))
       (message "Decode: %s" (lookup-key input-decode-map (kbd "M-O A")))
       (setq ret (looking-at "nulla pariatur. Excepteur sint occaecat cupidatat non proident,"))
       (kill-buffer (current-buffer)))
@@ -957,6 +965,7 @@ Selected mark would not be cleared after paste."
           ergoemacs-keyboard-layout old-ergoemacs-keyboard-layout)
     (ergoemacs-mode -1)
     (ergoemacs-mode 1)
+    ;; (progn (require 'ergoemacs-test) (ert "ergoemacs-test-terminal-M-O-fight"))
     (should ret)))
 
 (provide 'ergoemacs-test)
