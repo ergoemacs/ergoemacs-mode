dd7044951f6af7585659f010fe84b30367835ed0
Matthew L. Fidler <matthew.fidler@gmail.com>
2014-05-12 07:57:57 -0500

    Turn of message for modal keymap.

--- ergoemacs-shortcuts.el
+++ ergoemacs-shortcuts.el
@@ -269,7 +269,7 @@ universal argument can be entered.
                                      (lambda (x) x)
                                      (nth (+ 2 off) help-list) ", ")) "")))))
     (while (not ret)
-      (unless (minibufferp)
+      (unless (or (minibufferp) ergoemacs-modal)
         (let (message-log-max)
           (message "%s%s%s%s%s%s\t%s"
                    (if ergoemacs-describe-key
