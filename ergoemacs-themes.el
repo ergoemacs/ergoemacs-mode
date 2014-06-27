f51b081538627c451aa412e20d1f02845a334c28
Matthew L. Fidler <matthew.fidler@gmail.com>
2014-06-27 06:30:42 -0500

    Move the fixed bracket moving to the bracket theme component.

--- ergoemacs-themes.el
+++ ergoemacs-themes.el
@@ -162,8 +162,7 @@
   
   (global-set-key (kbd "<M-f4>") 'delete-frame) ;; Alt+f4 should work.
   
-  (global-set-key (kbd "<M-left>") 'ergoemacs-backward-open-bracket) ; Alt+←
-  (global-set-key (kbd "<M-right>") 'ergoemacs-forward-close-bracket) ; Alt+→
+   ; Alt+→
   (global-set-key (kbd "<M-up>") 'ergoemacs-backward-block) ; Alt+↑
   ;; Allow shift selection
   (global-set-key (kbd "<S-down-mouse-1>") 'mouse-save-then-kill)
@@ -503,7 +502,9 @@
 (ergoemacs-theme-component move-bracket ()
   "Move By Bracket"
   (global-set-key (kbd "M-J") 'ergoemacs-backward-open-bracket)
-  (global-set-key (kbd "M-L") 'ergoemacs-forward-close-bracket))
+  (global-set-key (kbd "M-L") 'ergoemacs-forward-close-bracket)
+  (global-set-key (kbd "<M-left>") 'ergoemacs-backward-open-bracket) ; Alt+←
+  (global-set-key (kbd "<M-right>") 'ergoemacs-forward-close-bracket))
 
 (ergoemacs-theme-component copy ()
   "Copy, Cut, Paste, Redo and Undo"
