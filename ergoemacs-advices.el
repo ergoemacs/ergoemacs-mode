8e4705489a509151ee2e11c7ba4cd091ab188d05
Matthew L. Fidler <matthew.fidler@gmail.com>
2014-08-18 09:15:18 -0500

    Possible Fix #286

--- ergoemacs-advices.el
+++ ergoemacs-advices.el
@@ -203,7 +203,9 @@ If `pre-command-hook' is used and `ergoemacs-mode' is enabled add to `ergoemacs-
    ((and (ignore-errors (symbolp function))
          (not (ignore-errors
                 (string= "ergoemacs-" (substring (symbol-name function) 0 10))))
-         (ergoemacs-is-user-defined-map-change-p function))
+         (and (not ergoemacs-changes-are-ignored-in-runtime)
+              (let (ergoemacs-is-user-defined-map-change-p)
+                (ergoemacs-is-user-defined-map-change-p function))))
     (fset (intern (concat "ergoemacs--user-" (symbol-name function)))
           `(lambda(&rest args)
              ,(format "Run `%s' respecting user keys in `ergoemacs-mode'."
