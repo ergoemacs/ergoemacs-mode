9eadbc060248ac20bc1f47b2c1f6f9661d464c70
Matthew L. Fidler <matthew.fidler@gmail.com>
2016-09-12 12:07:31 -0500

    Fix the russian layout more

--- ergoemacs-layouts.el
+++ ergoemacs-layouts.el
@@ -344,17 +344,19 @@
     "" ""  "Z" "X" "M" "C" "V" "K" "L" "<" ">" "?" "" "" "")
   "US Workman layout.  URL `http://www.workmanlayout.com/blog/'.")
 
-(defvar ergoemacs-layout-jcuken
+(defvar ergoemacs-layout-ru
   '("" "" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "-" "=" "\\"
-    "" ""  "й" "ц" "у" "к" "е" "н" "г" "ш" "щ" "з" "х" "ъ" "." ; Even though this isn't the position it allows translation.
+    "" ""  "й" "ц" "у" "к" "е" "н" "г" "ш" "щ" "з" "х" "ъ" "" 
     "" ""  "ф" "ы" "в" "а" "п" "р" "о" "л" "д" "ж" "э"  "" ""
     "" ""  "я" "ч" "с" "м" "и" "т" "ь" "б" "ю" "." "" "" ""
     ;; Shifted
     "" "Ë" "!" "\"" "№" ";" "%" ":" "?" "*" "(" ")" "_" "+" "/"
-    "" ""  "Й" "Ц" "У" "К" "Е" "Н" "Г" "Ш" "Щ" "З" "Х" "Ъ" "," ; Even though this isn't the position it allows QWERTY translation...
+    "" ""  "Й" "Ц" "У" "К" "Е" "Н" "Г" "Ш" "Щ" "З" "Х" "Ъ" "" 
     "" ""  "Ф" "Ы" "В" "А" "П" "Р" "О" "Л" "Д" "Ж" "Э" "" ""
     "" "" "Я" "Ч" "С" "М" "И" "Т" "Ь" "Б" "Ю" "," "" "" "")
   "Russian/Cryllic jcuken layout.")
+
+(defvaralias 'ergoemacs-layout-jcuken 'ergoemacs-layout-ru)
 
 (require 'help-mode)
 (defvar quail-keyboard-layout-alist)
