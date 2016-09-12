c9dc74b9ad91e42d3604ec24c587840db9de0be9
Matthew L. Fidler <matthew.fidler@gmail.com>
2016-09-12 09:39:10 -0500

    Drop duplicate qwerty

--- ergoemacs-layouts.el
+++ ergoemacs-layouts.el
@@ -344,18 +344,6 @@
     "" ""  "Z" "X" "M" "C" "V" "K" "L" "<" ">" "?" "" "" "")
   "US Workman layout.  URL `http://www.workmanlayout.com/blog/'.")
 
-(defvar ergoemacs-layout-us
-  '("" "`" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "-" "=" ""
-    "" ""  "q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "[" "]" "\\"
-    "" ""  "a" "s" "d" "f" "g" "h" "j" "k" "l" ";" "'" "" ""
-    "" ""  "z" "x" "c" "v" "b" "n" "m" "," "." "/" "" "" ""
-    ;; Shifted
-    "" "~" "!" "@" "#" "$" "%" "^" "&" "*" "(" ")" "_" "+" ""
-    "" ""  "Q" "W" "E" "R" "T" "Y" "U" "I" "O" "P" "{" "}" "|"
-    "" ""  "A" "S" "D" "F" "G" "H" "J" "K" "L" ":" "\"" "" ""
-    "" ""  "Z" "X" "C" "V" "B" "N" "M" "<" ">" "?" "" "" "")
-  "US English QWERTY layout.")
-
 (defvar ergoemacs-layout-jcuken
   '("" "" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "-" "=" "\\"
     "" ""  "й" "ц" "у" "к" "е" "н" "г" "ш" "щ" "з" "х" "ъ" "." ; Even though this isn't the position it allows translation.
