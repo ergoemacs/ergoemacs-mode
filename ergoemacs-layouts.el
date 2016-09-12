052c8da586eaa5017ce449bf64b194ff8855f27f
Matthew L. Fidler <matthew.fidler@gmail.com>
2016-09-12 07:49:25 -0500

    Add jcuken keyboard

--- ergoemacs-layouts.el
+++ ergoemacs-layouts.el
@@ -332,7 +332,41 @@
     "" ""  "Z" "X" "M" "C" "V" "K" "L" "<" ">" "?" "" "" "")
   "US Workman layout.  URL `http://www.workmanlayout.com/blog/'.")
 
+(defvar ergoemacs-layout-workman
+  '("" "`" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "-" "=" ""
+    "" ""  "й" "d" "r" "w" "b" "j" "f" "u" "p" ";" "[" "]" "\\"
+    "" ""  "a" "s" "h" "t" "g" "y" "n" "e" "o" "i" "'" "" ""
+    "" ""  "z" "x" "m" "c" "v" "k" "l" "," "." "/" "" "" ""
+    ;; Shifted
+    "" "~" "!" "@" "#" "$" "%" "^" "&" "*" "(" ")" "_" "+" ""
+    "" ""  "Q" "D" "R" "W" "B" "J" "F" "U" "P" ":" "{" "}" "|"
+    "" ""  "A" "S" "H" "T" "G" "Y" "N" "E" "O" "I" "\"" "" ""
+    "" ""  "Z" "X" "M" "C" "V" "K" "L" "<" ">" "?" "" "" "")
+  "US Workman layout.  URL `http://www.workmanlayout.com/blog/'.")
 
+(defvar ergoemacs-layout-us
+  '("" "`" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "-" "=" ""
+    "" ""  "q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "[" "]" "\\"
+    "" ""  "a" "s" "d" "f" "g" "h" "j" "k" "l" ";" "'" "" ""
+    "" ""  "z" "x" "c" "v" "b" "n" "m" "," "." "/" "" "" ""
+    ;; Shifted
+    "" "~" "!" "@" "#" "$" "%" "^" "&" "*" "(" ")" "_" "+" ""
+    "" ""  "Q" "W" "E" "R" "T" "Y" "U" "I" "O" "P" "{" "}" "|"
+    "" ""  "A" "S" "D" "F" "G" "H" "J" "K" "L" ":" "\"" "" ""
+    "" ""  "Z" "X" "C" "V" "B" "N" "M" "<" ">" "?" "" "" "")
+  "US English QWERTY layout.")
+
+(defvar ergoemacs-layout-jcuken
+  '("" "" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "-" "=" "\\"
+    "" ""  "й" "ц" "у" "к" "е" "н" "г" "ш" "щ" "з" "х" "ъ" "." ; Even though this isn't the position it allows translation.
+    "" ""  "ф" "ы" "в" "а" "п" "р" "о" "л" "д" "ж" "э"  "" ""
+    "" ""  "я" "ч" "с" "м" "и" "т" "ь" "б" "ю" "." "" "" ""
+    ;; Shifted
+    "" "Ë" "!" "\"" "#" ";" "%" ":" "?" "*" "(" ")" "_" "+" "/"
+    "" ""  "Й" "Ц" "У" "К" "Е" "Н" "Г" "Ш" "Щ" "З" "Х" "Ъ" "," ; Even though this isn't the position it allows QWERTY translation...
+    "" ""  "Ф" "Ы" "В" "А" "П" "Р" "О" "Л" "Д" "Ж" "\\" "" ""
+    "" ""  "Э" "Я" "Ч" "С" "М" "И" "Т" "Ь" "Б" "Ю№" "" "" "")
+  "Russian/Cryllic Lcuken layout.")
 
 (require 'help-mode)
 (defvar quail-keyboard-layout-alist)
