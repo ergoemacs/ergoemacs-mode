f3f7b100981828e881aaca186d2a196e8c08b593
Fidler <matthew.fidler@gmail.com>
2017-07-20 21:41:24 -0500

    Add keyboard layout;  See Issue #462

--- ergoemacs-layouts.el
+++ ergoemacs-layouts.el
@@ -30,6 +30,18 @@
 (eval-when-compile
   (require 'ergoemacs-macros))
 
+(defvar ergoemacs-layout-jp-mac-en
+  '("" "" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "-" "^" "¥"
+    "" "" "q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "@" "[" ""
+    "" "" "a" "s" "d" "f" "g" "h" "j" "k" "l" ";" ":" "]" "" ""
+    "" "z" "x" "c" "v" "b" "n" "m" "," "." "/" "_" "" ""
+    ;; Shifted
+    "" "" "!" "\"" "#" "$" "%" "&" "\'" "(" ")" "0" "=" "~" "|"
+    "" "" "Q" "W" "E" "R" "T" "Y" "U" "I" "O" "P" "" "{" ""
+    "" "" "A" "S" "D" "F" "G" "H" "J" "K" "L" "+" "*" "}" ""
+    "" "" "Z" "X" "C" "V" "B" "N" "M" "<" ">" "?" "_" "" "")
+  "JP English/Romaji QWERTY Mac Keyboard")`
+
 (defvar ergoemacs-layout-pt
   '("" "\\" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "'" "«" ""
     "" ""  "q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "+" "´" ""
