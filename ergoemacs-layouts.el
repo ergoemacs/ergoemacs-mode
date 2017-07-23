b70d9fe00c21063726745dde45f53b42168d0d21
MicroAnibal <developer.anibal.mendonca@gmail.com>
2017-07-23 19:11:30 +0100

    Fix  back quote.
    Corrects  void variable ergoemacs-layouts-pt

--- ergoemacs-layouts.el
+++ ergoemacs-layouts.el
@@ -40,7 +40,7 @@
     "" "" "Q" "W" "E" "R" "T" "Y" "U" "I" "O" "P" "" "{" ""
     "" "" "A" "S" "D" "F" "G" "H" "J" "K" "L" "+" "*" "}" ""
     "" "" "Z" "X" "C" "V" "B" "N" "M" "<" ">" "?" "_" "" "")
-  "JP English/Romaji QWERTY Mac Keyboard")`
+  "JP English/Romaji QWERTY Mac Keyboard")
 
 (defvar ergoemacs-layout-pt
   '("" "\\" "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" "'" "«" ""
